73ff0262a8be47b69ffd7bf4b8ab5d4419c2c29b
Aditya Eka Pranata <aepranata@gmail.com>
2022-04-13 08:16:56 +0700

    Revert "usb: gadget: rndis: add spinlock for rndis response list"
    This reverts commit 9f5d8ba538ef81cd86ea587ca3f8c77e26bea405.
    
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- drivers/usb/gadget/function/rndis.c
+++ drivers/usb/gadget/function/rndis.c
@@ -945,7 +945,6 @@ struct rndis_params *rndis_register(void (*resp_avail)(void *v), void *v,
 	params->flow_ctrl_enable = flow_ctrl_enable;
 	params->v = v;
 	INIT_LIST_HEAD(&params->resp_queue);
-	spin_lock_init(&params->resp_lock);
 	pr_debug("%s: configNr = %d\n", __func__, i);
 
 	return params;
@@ -1133,7 +1132,6 @@ u8 *rndis_get_next_response(struct rndis_params *params, u32 *length)
 	}
 	spin_unlock_irqrestore(&params->lock, flags);
 
-	spin_unlock(&params->resp_lock);
 	return NULL;
 }
 EXPORT_SYMBOL_GPL(rndis_get_next_response);
